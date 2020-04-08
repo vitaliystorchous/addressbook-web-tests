2996a6553573c70b90e1eb0f96b92dcd7de2f8b5
vitaliystorchous <50746287+vitaliystorchous@users.noreply.github.com>
2020-04-08 07:50:30 +0300

    добавлена провера в тест SetCustomPageAsHomepage
    проверка наличия custom page

--- src/test/java/ru/stqa/pft/addressbook/tests/SetCustomPageAsHomepage.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/SetCustomPageAsHomepage.java
@@ -7,6 +7,10 @@ public class SetCustomPageAsHomepage extends TestBase {
     @Test
     public void theTest() {
         app.getNavigationHelper().goToPagesPage();
+        if(! app.getMenuEditorHelper().isElementPresent(app.customPageName)) {
+            app.getMenuEditorHelper().createCustomPage(app.customPageName);
+            app.getNavigationHelper().goToPagesPage();
+        }
         app.getMenuEditorHelper().setSelectedPageAsHomepage(app.customPageName);
     }
 }
