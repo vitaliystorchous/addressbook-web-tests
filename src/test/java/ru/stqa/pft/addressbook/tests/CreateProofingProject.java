6c81295f9b83a4666f58b7fe004e210d7496498c
vitaliystorchous <50746287+vitaliystorchous@users.noreply.github.com>
2020-04-14 07:24:48 +0300

    добавлено сравнение упорядоченых списков
    CreateProofingProject

--- src/test/java/ru/stqa/pft/addressbook/tests/CreateProofingProject.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/CreateProofingProject.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.MenuEditorItem;
 import ru.stqa.pft.addressbook.model.MenuEditorItem.Type;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class CreateProofingProject extends TestBase {
@@ -13,11 +14,18 @@ public class CreateProofingProject extends TestBase {
     public void theTest() {
         app.getNavigationHelper().goToPagesPage();
         List<MenuEditorItem> before = app.getMenuEditorHelper().getMenuItemsList();
-        app.getMenuEditorHelper().createMenuEditorItem(new MenuEditorItem(Type.PROOFING_PROJECT, app.proofingProjectName));
+        MenuEditorItem item = new MenuEditorItem(Type.PROOFING_PROJECT, app.proofingProjectName);
+        app.getMenuEditorHelper().createMenuEditorItem(item);
         app.getPageEditorHelper().waitPageEditorOpened();
         app.getNavigationHelper().goToPagesPage();
         List<MenuEditorItem> after = app.getMenuEditorHelper().getMenuItemsList();
         Assert.assertEquals(after.size(), before.size() + 1);
+
+        before.add(item);
+        Comparator<? super MenuEditorItem> byId = (i1, i2) -> Integer.compare(i1.getDataId(), i2.getDataId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(after, before);
     }
 
 }
