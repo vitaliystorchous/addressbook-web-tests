bb5f362f8878a72bb3006f71b666a7dcc2f5ed48
vitaliystorchous <50746287+vitaliystorchous@users.noreply.github.com>
2020-04-14 07:21:30 +0300

    добавлено сравнение упорядоченых списков
    CreateExternalLink

--- src/test/java/ru/stqa/pft/addressbook/tests/CreateExternalLink.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/CreateExternalLink.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.MenuEditorItem;
 import ru.stqa.pft.addressbook.model.MenuEditorItem.Type;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class CreateExternalLink extends TestBase {
@@ -13,10 +14,17 @@ public class CreateExternalLink extends TestBase {
     public void theTest() {
         app.getNavigationHelper().goToPagesPage();
         List<MenuEditorItem> before = app.getMenuEditorHelper().getMenuItemsList();
-        app.getMenuEditorHelper().createMenuEditorItem(new MenuEditorItem(Type.EXTERNAL_LINK, app.externalLinkName));
+        MenuEditorItem item = new MenuEditorItem(Type.EXTERNAL_LINK, app.externalLinkName);
+        app.getMenuEditorHelper().createMenuEditorItem(item);
         app.pause(2);
         List<MenuEditorItem> after = app.getMenuEditorHelper().getMenuItemsList();
         Assert.assertEquals(after.size(), before.size() + 1);
+
+        before.add(item);
+        Comparator<? super MenuEditorItem> byId = (i1, i2) -> Integer.compare(i1.getDataId(), i2.getDataId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(after, before);
     }
 
 }
