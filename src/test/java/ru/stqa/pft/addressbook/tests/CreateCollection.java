26278f3096aa4fe2cf0d675c6d4ef3eea5c7b157
vitaliystorchous <50746287+vitaliystorchous@users.noreply.github.com>
2020-04-16 20:52:49 +0300

    переделан тест CreateCollection (использование Set вместо List)

--- src/test/java/ru/stqa/pft/addressbook/tests/CreateCollection.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/CreateCollection.java
@@ -7,22 +7,21 @@ import ru.stqa.pft.addressbook.model.MenuEditorItem.Type;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
 
 public class CreateCollection extends TestBase {
 
     @Test
     public void theTest() {
         app.goTo().pagesPage();
-        List<MenuEditorItem> before = app.menuEditor().itemsList();
-        MenuEditorItem item = new MenuEditorItem().withType(Type.COLLECTION).withName(app.collectionName);
-        app.menuEditor().createItem(item);
-        List<MenuEditorItem> after = app.menuEditor().itemsList();
+        Set<MenuEditorItem> before = app.menuEditor().allItems();
+        MenuEditorItem collection = new MenuEditorItem().withType(Type.COLLECTION).withName(app.collectionName);
+        app.menuEditor().createItem(collection);
+        Set<MenuEditorItem> after = app.menuEditor().allItems();
         Assert.assertEquals(after.size(), before.size() + 1);
 
-        before.add(item);
-        Comparator<? super MenuEditorItem> byId = (i1, i2) -> Integer.compare(i1.getDataId(), i2.getDataId());
-        before.sort(byId);
-        after.sort(byId);
+        collection.withDataId(after.stream().mapToInt(MenuEditorItem::getDataId).max().getAsInt());
+        before.add(collection);
         Assert.assertEquals(after, before);
     }
 
