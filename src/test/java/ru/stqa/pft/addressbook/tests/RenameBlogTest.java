eee7712816978858b7b98525293fcd262ee7fed2
vitaliystorchous <50746287+vitaliystorchous@users.noreply.github.com>
2020-04-18 15:37:33 +0300

    added test RenameBlogTest

---
+++ src/test/java/ru/stqa/pft/addressbook/tests/RenameBlogTest.java
@@ -0,0 +1,32 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.MenuEditorItem;
+
+import java.util.Set;
+
+public class RenameBlogTest extends TestBase {
+
+    @BeforeMethod
+    public void ensurePreconditions() {
+        app.goTo().pagesPage();
+        if(! app.menuEditor().isItemPresent(MenuEditorItem.Type.BLOG)) {
+            app.menuEditor().createItem(new MenuEditorItem().withType(MenuEditorItem.Type.BLOG).withName(app.blogName));
+        }
+    }
+
+    @Test
+    public void testRenameCustomPage() {
+        Set<MenuEditorItem> before = app.menuEditor().allItems();
+        MenuEditorItem renamedBlog = MenuEditorItem.getItem(before, MenuEditorItem.Type.BLOG);
+        before.remove(renamedBlog);
+        before.add(renamedBlog.withName("Renamed " + renamedBlog.getName()));
+        app.menuEditor().renameItem(renamedBlog); // здесь нужно переделать метод что бы с помощью него я мог переименовать любую item в menu editor и после этого переименовать метод на renameItem
+        Set<MenuEditorItem> after = app.menuEditor().allItems();
+        Assert.assertEquals(after.size(), before.size());
+
+        Assert.assertEquals(after, before);
+    }
+}
