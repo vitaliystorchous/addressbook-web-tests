40106f77c942828bc2af00984b38d226ee6bcee3
vitaliystorchous <50746287+vitaliystorchous@users.noreply.github.com>
2020-04-18 15:40:10 +0300

    added test RenameCollectionTest

---
+++ src/test/java/ru/stqa/pft/addressbook/tests/RenameCollectionTest.java
@@ -0,0 +1,32 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.MenuEditorItem;
+
+import java.util.Set;
+
+public class RenameCollectionTest extends TestBase {
+
+    @BeforeMethod
+    public void ensurePreconditions() {
+        app.goTo().pagesPage();
+        if(! app.menuEditor().isItemPresent(MenuEditorItem.Type.COLLECTION)) {
+            app.menuEditor().createItem(new MenuEditorItem().withType(MenuEditorItem.Type.COLLECTION).withName(app.collectionName));
+        }
+    }
+
+    @Test
+    public void testRenameCustomPage() {
+        Set<MenuEditorItem> before = app.menuEditor().allItems();
+        MenuEditorItem renamedCollection = MenuEditorItem.getItem(before, MenuEditorItem.Type.COLLECTION);
+        before.remove(renamedCollection);
+        before.add(renamedCollection.withName("Renamed " + renamedCollection.getName()));
+        app.menuEditor().renameItem(renamedCollection); // здесь нужно переделать метод что бы с помощью него я мог переименовать любую item в menu editor и после этого переименовать метод на renameItem
+        Set<MenuEditorItem> after = app.menuEditor().allItems();
+        Assert.assertEquals(after.size(), before.size());
+
+        Assert.assertEquals(after, before);
+    }
+}
