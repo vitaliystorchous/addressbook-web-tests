d7c3279b6ab5d0984c8fac1f82b6811eee9229d9
vitaliystorchous <50746287+vitaliystorchous@users.noreply.github.com>
2020-04-20 11:44:15 +0300

    modified DeleteExternalLinkTest

--- src/test/java/ru/stqa/pft/addressbook/tests/DeleteCustomPageTest.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/DeleteCustomPageTest.java
@@ -8,6 +8,7 @@ import ru.stqa.pft.addressbook.model.MenuEditorItem;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertEquals;
 import static ru.stqa.pft.addressbook.model.MenuEditorItem.Type.CUSTOM_PAGE;
 
 public class DeleteCustomPageTest extends TestBase {
@@ -25,7 +26,7 @@ public class DeleteCustomPageTest extends TestBase {
         Items before = app.menuEditor().allItems();
         MenuEditorItem customPage = MenuEditorItem.getItem(before, CUSTOM_PAGE);
         app.menuEditor().deleteItem(customPage);
-        Assert.assertEquals(app.menuEditor().itemsCount(), before.size() - 1);
+        assertEquals(app.menuEditor().itemsCount(), before.size() - 1);
         Items after = app.menuEditor().allItems();
         assertThat(after, equalTo(before.without(customPage)));
     }
--- src/test/java/ru/stqa/pft/addressbook/tests/DeleteExternalLinkTest.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/DeleteExternalLinkTest.java
@@ -1,8 +1,11 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.Items;
 import ru.stqa.pft.addressbook.model.MenuEditorItem;
 
 import java.util.Set;
@@ -21,13 +24,11 @@ public class DeleteExternalLinkTest extends TestBase {
 
     @Test
     public void test() {
-        Set<MenuEditorItem> before = app.menuEditor().allItems();
+        Items before = app.menuEditor().allItems();
         MenuEditorItem externalLink = MenuEditorItem.getItem(before, EXTERNAL_LINK);
         app.menuEditor().deleteItem(externalLink);
-        Set<MenuEditorItem> after = app.menuEditor().allItems();
-        Assert.assertEquals(after.size(), before.size() - 1);
-
-        before.remove(externalLink);
-        Assert.assertEquals(after, before);
+        Assert.assertEquals(app.menuEditor().itemsCount(), before.size() - 1);
+        Items after = app.menuEditor().allItems();
+        MatcherAssert.assertThat(after, CoreMatchers.equalTo(before.without(externalLink)));
     }
 }
