02903e481948c384d52b99ac5c200f8bb208eed7
vitaliystorchous <50746287+vitaliystorchous@users.noreply.github.com>
2020-04-20 10:49:25 +0300

    modified CreateExternalLinkTest

--- src/test/java/ru/stqa/pft/addressbook/tests/CreateCollectionTest.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/CreateCollectionTest.java
@@ -1,8 +1,5 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.Items;
 import ru.stqa.pft.addressbook.model.MenuEditorItem;
--- src/test/java/ru/stqa/pft/addressbook/tests/CreateExternalLinkTest.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/CreateExternalLinkTest.java
@@ -1,26 +1,25 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.Items;
 import ru.stqa.pft.addressbook.model.MenuEditorItem;
-import ru.stqa.pft.addressbook.model.MenuEditorItem.Type;
 
-import java.util.Set;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static ru.stqa.pft.addressbook.model.MenuEditorItem.Type.EXTERNAL_LINK;
 
 public class CreateExternalLinkTest extends TestBase {
 
     @Test
     public void theTest() {
         app.goTo().pagesPage();
-        Set<MenuEditorItem> before = app.menuEditor().allItems();
-        MenuEditorItem externalLink = new MenuEditorItem().withType(Type.EXTERNAL_LINK).withName(app.externalLinkName);
+        Items before = app.menuEditor().allItems();
+        MenuEditorItem externalLink = new MenuEditorItem().withType(EXTERNAL_LINK).withName(app.externalLinkName);
         app.menuEditor().createItem(externalLink);
-        Set<MenuEditorItem> after = app.menuEditor().allItems();
-        Assert.assertEquals(after.size(), before.size() + 1);
-
-        externalLink.withDataId(after.stream().mapToInt(MenuEditorItem::getDataId).max().getAsInt());
-        before.add(externalLink);
-        Assert.assertEquals(after, before);
+        assertThat(app.menuEditor().getItemsCount(), equalTo(before.size() + 1));
+        Items after = app.menuEditor().allItems();
+        assertThat(after, equalTo(
+                before.withAdded(externalLink.withDataId(after.stream().mapToInt(MenuEditorItem::getDataId).max().getAsInt()))));
     }
 
 }
