f6a6efe50849e4c19ac0c2c7cb763cbea6d59c28
vitaliystorchous <50746287+vitaliystorchous@users.noreply.github.com>
2020-04-20 12:22:23 +0300

    modified RenameBlogTest

--- src/test/java/ru/stqa/pft/addressbook/tests/RenameBlogTest.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/RenameBlogTest.java
@@ -1,13 +1,19 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.Items;
 import ru.stqa.pft.addressbook.model.MenuEditorItem;
 
 import java.util.Set;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertEquals;
 import static ru.stqa.pft.addressbook.model.MenuEditorItem.Type.BLOG;
 
 public class RenameBlogTest extends TestBase {
@@ -22,15 +28,13 @@ public class RenameBlogTest extends TestBase {
 
     @Test
     public void test() {
-        Set<MenuEditorItem> before = app.menuEditor().allItems();
-        MenuEditorItem renamedBlog = MenuEditorItem.getItem(before, BLOG);
-        before.remove(renamedBlog);
-        before.add(renamedBlog.withName("Renamed test blog (*Selenium*)"));
-        app.menuEditor().renameItem(renamedBlog);
-        Set<MenuEditorItem> after = app.menuEditor().allItems();
-        Assert.assertEquals(after.size(), before.size());
-
-        Assert.assertEquals(after, before);
+        Items before = app.menuEditor().allItems();
+        MenuEditorItem blogBefore = MenuEditorItem.getItem(before, BLOG);
+        MenuEditorItem blogAfter = blogBefore.withName("Renamed test blog (*Selenium*)");
+        app.menuEditor().renameItem(blogBefore);
+        assertEquals(app.menuEditor().itemsCount(), before.size());
+        Items after = app.menuEditor().allItems();
+        assertThat(after, equalTo(before.without(blogBefore).withAdded(blogAfter)));
     }
 
     @AfterMethod
