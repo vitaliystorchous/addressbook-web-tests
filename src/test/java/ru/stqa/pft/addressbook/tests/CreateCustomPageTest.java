94aa1642c450533bb1ed3ab177f165bbbb2fc329
vitaliystorchous <50746287+vitaliystorchous@users.noreply.github.com>
2020-04-21 09:15:29 +0300

    в проект добавлен логгер

--- src/test/java/ru/stqa/pft/addressbook/tests/CreateCustomPageTest.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/CreateCustomPageTest.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.addressbook.tests;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.Items;
@@ -12,16 +14,16 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static ru.stqa.pft.addressbook.model.MenuEditorItem.Type.valueOf;
 
 public class CreateCustomPageTest extends TestBase {
+
+
 
     @DataProvider
     public Iterator<Object[]> validItemsFromXmlFromJson() throws IOException {
@@ -56,7 +58,7 @@ public class CreateCustomPageTest extends TestBase {
     }
 
     @Test (dataProvider = "validItemsFromXmlFromJson")
-    public void test(MenuEditorItem customPage) {
+    public void createCustomPage(MenuEditorItem customPage) {
         app.goTo().pagesPage();
         Items before = app.menuEditor().allItems();
         app.menuEditor().createItem(customPage);
--- src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,23 +1,41 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class TestBase {
 
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+
     protected static final ApplicationManager app
-            = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+            = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeSuite
     public void setUp() throws Exception {
         app.init();
     }
 
-    @AfterSuite
+    @AfterSuite (alwaysRun = true)
     public void tearDown() {
         app.stop();
     }
 
+    @BeforeMethod
+    public void logTestStart(Method method, Object[] p) {
+        logger.info("Start test " + method.getName() + " with parameters " + Arrays.asList(p));
+    }
+
+    @AfterMethod (alwaysRun = true)
+    public void logTestStop(Method method) {
+        logger.info("Stop test " + method.getName());
+    }
 }
