eb6849bf0cb4e382da04602e0367bc3db9a58377
vitaliystorchous <50746287+vitaliystorchous@users.noreply.github.com>
2020-05-01 16:36:50 +0300

    тестовый тригер запуска тестов с помощью jenkins

--- src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -71,7 +71,7 @@ public class ApplicationManager {
         } else {
             DesiredCapabilities capabilities = new DesiredCapabilities();
             capabilities.setBrowserName(browser);
-            capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "all")));
+            capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "windows")));
             wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
         }
 
