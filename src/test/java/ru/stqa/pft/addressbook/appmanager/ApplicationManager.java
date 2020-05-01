68b755dd747d951f1cf14fb3829659be4516806d
vitaliystorchous <50746287+vitaliystorchous@users.noreply.github.com>
2020-05-01 13:11:25 +0300

    заменено дефолтное значение платформы с win10 на all

--- src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -71,7 +71,7 @@ public class ApplicationManager {
         } else {
             DesiredCapabilities capabilities = new DesiredCapabilities();
             capabilities.setBrowserName(browser);
-            capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win10")));
+            capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "all")));
             wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
         }
 
